d1ba49c27cafc6ea9458a011685542110200aa72
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 13:29:29 -0300

    task 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,6 +1,5 @@
 import java.text.NumberFormat;
 import java.util.Locale;
-
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
@@ -9,7 +8,6 @@ public class PrintFormatacaoNumeros {
         String valorFormatado = numeroFormatado.format(valor);
         String valorreplace = ((valorFormatado.replace(".",",")));
         String valorreplace2 = valorreplace.substring(0,8) + "." + valorreplace.substring(9);
-
         System.out.printf("Valor:%s\n" ,valorreplace2);
         System.out.printf("Taxa: %.2f%%" , taxa);
     }
