d1b762d506cfabb984481992770b0a61bdd7f6e8
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 13:56:12 -0300

    task 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -10,6 +10,7 @@ public class PrintFormatacaoNumeros {
         String valorreplace = ((valorFormatado.replace(".",",")));
         String valorreplace2 = valorreplace.substring(0,8) + "." + valorreplace.substring(9);
         System.out.print("Valor: " + valorreplace2 + "\n");
-        System.out.printf("Taxa: %.2f%%" , taxa);
+        System.out.printf("Taxa: %.2f" , taxa);
+        System.out.println("%");
     }
 }
