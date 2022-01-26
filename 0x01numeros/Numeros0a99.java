8d320668246959a3065de821a0d42b5b1f645d0d
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 16:11:36 -0300

    task 0

---
+++ 0x01numeros/Numeros0a99.java
@@ -0,0 +1,11 @@
+public class Numeros0a99 {
+
+    public static void main(String[] args) {
+
+        for ( int numero = 0; numero < 100; numero ++) {
+
+            System.out.print(numero + ",");
+        }
+
+    }
+}
