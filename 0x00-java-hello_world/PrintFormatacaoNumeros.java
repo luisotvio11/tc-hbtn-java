cb3f6f355c4baea500f4ae0b75924d7754f7f0a8
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 11:11:09 -0300

    task 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,22 +1,7 @@
-import java.text.NumberFormat;
-import java.util.Locale;
-    public class PrintFormatacaoNumeros {
-
-
-        public static void main(String[] args) {
-            float taxa = 0.2456f;
-            float valor = 7654.321f;
-            //   NumberFormat numeroFormatado = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
-            // String valorFormatado = numeroFormatado.format(valor);
-            //System.out.printf("Valor: %s\n" ,valorFormatado);
-
-
-            // Locale moeda = new Locale("pt","BR");
-            String valorFormato = NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(valor);
-
-            System.out.printf("Valor: %s\n", valorFormato);
-            System.out.printf("Taxa: %.2f", taxa);
-            System.out.println("%");
-        }
-
+public class PrintFormatacaoNumeros {
+    public static void main(String[] args) {
+        float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.printf("Valor: " + String.format("R$ %,.2f",valor) + "\nTaxa: %.2f%%\n",taxa);
     }
+}
