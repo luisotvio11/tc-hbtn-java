865af6c1b2893d1781178fdd6ceafa6b37f699bc
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 17:34:57 -0300

    task 1

---
+++ 0x01/numeros/NumerosPares.java
@@ -0,0 +1,31 @@
+public class NumerosPares {
+
+
+            public static void main(String[] args) {
+
+                for ( int i = 0; i <100; i++) {
+                    if (i==98) {
+                        System.out.print(i+ "\n");
+                    }
+                    else if (i%2 == 0){
+                        System.out.print(i+ ", ");
+                    }
+                }
+
+            }
+
+
+        }
+
+
+
+
+
+
+            }
+
+        }
+
+    }
+
+
---
+++ Teste.java
@@ -0,0 +1,21 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        for ( int i = 0; i <100; i++) {
+            if (i==98) {
+                System.out.print(i+ "\n");
+            }
+            else if (i%2 == 0){
+                System.out.print(i+ ", ");
+            }
+        }
+
+    }
+
+
+    }
+
+
+
+
