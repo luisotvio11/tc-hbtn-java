aed1daab05b8aab0c5a026594d8a8351072fd8d5
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 18:29:11 -0300

    task 3

--- 0x01/maiusculo/Program.java
+++ 0x01/maiusculo/Program.java
@@ -1,4 +1,4 @@
-public class Program {
+public class Program{
 
     public static void main(String[] args) {
 
---
+++ 0x01/somar/Numero.java
@@ -0,0 +1,9 @@
+public class Numero {
+
+    public static int somar(int a, int b) {
+
+        return a + b;
+
+    }
+
+}
