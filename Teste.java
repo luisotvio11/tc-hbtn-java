53e4578d910e48a1f74efed58af32267e19057db
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-26 13:26:06 -0300

    task 5

--- Teste.java
+++ Teste.java
@@ -1,7 +1,6 @@
 import java.text.NumberFormat;
 import java.util.Locale;
-
-public class PrintFormatacaoNumeros {
+public class Teste {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
