c0013e0e27881ccb7686da1a59fdccee5d59fc8b
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-27 11:43:02 -0300

    task 1

--- 0x01/somar/Program.java
+++ 0x01/somar/Program.java
@@ -1,9 +1,9 @@
 public class Program {
 
     public static void main(String[] args) {
-        System.out.printf("%d\n", Numero.somar(24, 32));
-        System.out.printf("%d\n", Numero.somar(5, 0));
-        System.out.printf("%d\n", Numero.somar(4, -2));
+        System.out.printf("%d\n", Numero2.somar(24, 32));
+        System.out.printf("%d\n", Numero2.somar(5, 0));
+        System.out.printf("%d\n", Numero2.somar(4, -2));
     }
 }
 
---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,18 @@
+
+public class Quadrado {
+
+	public static double area (double lado) {
+			
+			
+		if (lado < 0) 
+			
+			throw new IllegalArgumentException("Lado deve possuir valor positivo");
+		
+			return lado * lado;
+
+	}
+	
+	
+	
+	
+}
