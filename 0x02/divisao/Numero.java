e0b0ddd41132388e254607ca2f791e508e3156a9
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-27 11:10:37 -0300

    task 0

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,23 @@
+public class Numero {
+	
+	public static void dividir(int a, int b) {
+
+		int resultado = 0;
+        
+        try {
+
+           resultado = a / b;
+
+        } catch (ArithmeticException E) {
+
+
+            System.out.println("Nao eh possivel dividir por zero");
+        } finally {
+        	
+            System.out.println(a + " / " + b + " = " + resultado);
+        }
+    }
+
+	
+
+}
