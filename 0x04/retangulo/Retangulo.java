6159a0d81a0e89293289b22ca76073d6b1ecf541
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-31 11:54:50 -0300

    task 2

--- 0x04/retangulo/Retangulo.java
+++ 0x04/retangulo/Retangulo.java
@@ -1,39 +1,26 @@
-public class Retangulo  extends FormaGeometrica{
+public class Retangulo extends FormaGeometrica {
+    private double largura;
+    private double altura;
 
-	private  double largura;
-	private double altura;
-	
-	
-	public double getLargura() {
-		return largura;
-	}
-	public void setLargura(double largura) {
-		
-		
-	if (this.largura < 0) {
-				
-				
-	throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
-				
-	}
-		
-		this.largura = largura;
-	}
-	public double getAltura() {
-		return altura;
-	}
-	public void setAltura(double altura) {
-		
-		if (this.altura < 0) {
-			
-			throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
-		}
-		
-		
-		this.altura = altura;
-	}
-	
-	
-	
-	
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        if (largura < 0) {
+            throw new IllegalArgumentException ("Largura deve ser maior ou igual a 0");
+        }
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        if (altura < 0){
+            throw new IllegalArgumentException ("Altura deve ser maior ou igual a 0");
+        }
+        this.altura = altura;
+    }
 }
