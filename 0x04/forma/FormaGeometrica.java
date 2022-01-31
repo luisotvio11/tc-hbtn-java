589f77c9b1316054250f8a448e844948ceee2b87
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-31 11:16:18 -0300

    task 1

---
+++ 0x04/forma/FormaGeometrica.java
@@ -0,0 +1,25 @@
+
+public class FormaGeometrica {
+	
+	
+	public double area (double a ,double b) {
+		
+		try {
+			
+			
+			
+		} catch (UnsupportedOperationException ex) {
+			
+			
+			System.out.println("Funcao Area nao implementada");
+		}
+		
+		
+		return a * b;
+
+	}
+	
+	
+	
+
+}
