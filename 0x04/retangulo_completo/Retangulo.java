1066c47af9b52ac74833c27b1f0b5fc6144bd289
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-01-31 13:04:56 -0300

    task 3

--- 0x04/retangulo_completo/Retangulo.java
+++ 0x04/retangulo_completo/Retangulo.java
@@ -1,39 +1,41 @@
-public class Retangulo extends FormaGeometrica {
-    private double largura;
-    private double altura;
+public class Retangulo extends FormaGeometrica{
 
-    public double getLargura() {
-        return largura;
-    }
+	private double largura;
+	private double altura;
+	
+	@Override
+	public double area() {
+		return largura * altura;
+	}	
 
-    public void setLargura(double largura) {
-        if (largura < 0) {
-            throw new IllegalArgumentException ("Largura deve ser maior ou igual a 0");
-        }
-        this.largura = largura;
-    }
+	public double getLargura() {
+		return largura;
+	}
+	public void setLargura(double largura) {
+		
+		if (largura < 0) {
+			throw new IllegalArgumentException ("Largura deve ser maior ou igual a 0");
+		}else {
+			this.largura = largura;
+		}
+			
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		if (altura < 0) {
+			throw new IllegalArgumentException ("Altura deve ser maior ou igual a 0");
+		}else {
+			this.altura = altura;
+		}
+	}
 
-    public double getAltura() {
-        return altura;
-    }
+	@Override
+	public String toString() {
+		String frm = String.format("[Retangulo] %.2f / %.2f", largura, altura);
+		return frm;
+	}
 
-    public void setAltura(double altura) {
-        if (altura < 0){
-            throw new IllegalArgumentException ("Altura deve ser maior ou igual a 0");
-        }
-        this.altura = altura;
-    }
-    
-    public double area () {
-    	
-    	return largura * altura;
-    	
-    	
-    }
-    
-    public String toString () {
-    	
-    	
-    		return String.format("[Retangulo] %.2f / %.2f", largura, altura);
-    }
 }
+
