ce54359062c2d831726112d336bd3b1501838b2b
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-01 10:20:45 -0300

    task 1

--- 0x05/livraria/exceptions/AutorInvalidoException.java
+++ 0x05/livraria/exceptions/AutorInvalidoException.java
@@ -1,10 +1,13 @@
+package exceptions;
+	
+
 public class AutorInvalidoException extends Exception {
 
-	
-		public  AutorInvalidoException (String mensagem) {
-			
-				super (mensagem);
-			
-		}
-	
+
+	public  AutorInvalidoException (String mensagem) {
+		
+			super (mensagem);
+		
+	}
+
 }
