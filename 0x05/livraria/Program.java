adee70d64a804d3a47346be859f795e934461956
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-01 10:26:13 -0300

    task 1

--- 0x05/livraria/Program.java
+++ 0x05/livraria/Program.java
@@ -1,6 +1,11 @@
+import java.util.Locale;
+
+
 public class Program {
 	
     public static void main(String[] args) {
+
+        Locale.setDefault(new Locale("pt", "BR"));
 
         try {
                 Livro livro1 = new Livro(
