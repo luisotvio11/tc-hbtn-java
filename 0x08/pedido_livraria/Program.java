0ec083ab5b9570ea1123f25040fa50d3a5739e63
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-04 12:38:03 -0300

    task 0

--- 0x08/pedido_livraria/Program.java
+++ 0x08/pedido_livraria/Program.java
@@ -8,13 +8,14 @@ public class Program {
 	public static void main(String[] args) {
 
 		Locale.setDefault(new Locale("pt", "BR"));
+		
 
 		Livro livro1 = new Livro("Duna", 2017, "Brasil", 56.30, 680, "Frank Herbert", 1);
-		Livro livro2 = new Livro("Um de nós está mentindo", 2018, "Brasil", 34.93, 384, "Karen Mcmanus", 1);
-		Livro livro3 = new Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272, "José Roberto Marques", 1);
+		Livro livro2 = new Livro("Um de nos esta mentindo", 2018, "Brasil", 34.93, 384, "Karen Mcmanus", 1);
+		Livro livro3 = new Livro("Mindset Milionário", 2021, "Brasil", 31.70, 272, "Jose Roberto Marques", 1);
 
 		Dvd dvd1 = new Dvd("Anjos da Noite 5 - Guerras de Sangue", 2016, "Estados Unidos", 16.90, "Anna Foerster",
-				"Ação", 91);
+				"Acao", 91);
 		Dvd dvd2 = new Dvd("Annabelle 2 - A Criação do Mal", 2017, "Brasil", 39.90, "Jame Wan e Peter Safran", "Terror",
 				109);
 
