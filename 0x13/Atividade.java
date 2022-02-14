743832155fbf7597d353e6dff2aaf3d94a563065
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-14 16:06:13 -0300

    task 0

---
+++ 0x13/Atividade.java
@@ -0,0 +1,7 @@
+package atividade;
+
+public interface Atividade {
+	
+	void executar ();
+
+}
---
+++ 0x13/CarregarVideo.java
@@ -0,0 +1,12 @@
+package atividade;
+
+public class CarregarVideo implements Atividade {
+
+	@Override
+	public void executar() {
+
+		System.out.println("carregando video");
+
+	}
+
+}
