fc82772a87f07cf652cc9a73dc953bfce5748eff
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-22 11:43:11 -0300

    task 0

---
+++ 0x15/blog/Blog.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Blog {
+	
+	public List<Post> posts;
+
+	
+	public Blog () {
+		
+		posts = new ArrayList<Post>();
+	}
+	
+	public void adicionarPostagem (Post post) {
+		
+		posts.add (post);
+	}
+	
+	public Set <String> obterTodosAutores () {
+		
+		Set<String> autores = new TreeSet <> ();
+		
+	
+		for (Post post : posts) {
+			
+			autores.add(post.getAutor());
+		}
+		
+		return autores;
+	
+	}
+	
+	public Map <String, Integer> obterContagemPorCategoria () {
+		
+		Map	<String, Integer> contagemPorCategoria = new TreeMap <> ();
+		
+		int contagemAtual;
+		
+		for (Post post: posts) {
+			
+			if (contagemPorCategoria.containsKey(post.getCategoria())){
+				
+				contagemAtual = contagemPorCategoria.get(post.getCategoria());
+			}
+			else {
+				
+				contagemAtual = 0;
+			}
+			
+			contagemPorCategoria.put (post.getCategoria(), contagemAtual + 1);
+		}
+		
+		return contagemPorCategoria;
+	}
+	
+
+	
+	
+
+}
