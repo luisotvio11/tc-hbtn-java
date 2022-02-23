e7861cf0554a045a8d25f7b1bb0604fa19067709
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-23 16:05:05 -0300

    task 0

--- 0x17/consulta_produtos/ConsultaProdutos.java
+++ 0x17/consulta_produtos/ConsultaProdutos.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ConsultaProdutos {
 
 	public static List<Produto> filtrar(List<Produto> produtos, CriterioFiltro criterio) {
+
+		Locale.setDefault(new Locale("pt", "BR"));
 
 		ArrayList<Produto> produtosFiltrados = new ArrayList<Produto>();
 
