23cb9f977261a79476bdbba1293aff220f5ecbed
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-25 18:02:31 -0300

    task 4

---
+++ 0x18/desconto_eletronicos/Consulta.java
@@ -0,0 +1,35 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+	public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+
+		return pedido.getProdutos().stream().filter(p -> p.getCategoria() == CategoriaProduto.LIVRO).toList();
+	}
+
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+
+		return produtos.stream().max(Comparator.comparing(Produto::getPreco)).get();
+	}
+
+	public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double valorProduto) {
+
+		return produtos.stream().filter(p -> p.getPreco() >= valorProduto).collect(Collectors.toList());
+	}
+
+	public static List<Pedido> obterPedidosComEletronicos(List<Pedido> listaPedidos) {
+		return listaPedidos.stream()
+				.filter(p -> p.getProdutos().stream().anyMatch(e -> e.getCategoria() == CategoriaProduto.ELETRONICO))
+				.toList();
+	}
+
+	public static List<Produto> aplicar15PorcentoDescontoEletronicos(List<Produto> produto) {
+		return produto.stream().map(p -> {
+			if (p.getCategoria() == CategoriaProduto.ELETRONICO)
+				p.setPreco(p.getPreco() - (p.getPreco() * 15 / 100));
+			return p;
+		}).collect(Collectors.toList());
+	}
+}
