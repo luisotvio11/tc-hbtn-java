089f720f9c35769d98c913fe91f3c90a7137ff9b
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-02-25 16:24:11 -0300

    task 0

---
+++ 0x18/livros/Consulta.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Consulta {
+	
+	public static List <Produto> obterLivrosDoPedido(Pedido pedido){
+		
+		return pedido.getProdutos()
+				.stream()
+				.filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+				.toList();
+	}
+
+}
