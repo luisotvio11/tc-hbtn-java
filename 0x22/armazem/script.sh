41ab3ef7d3f8688ba8abfce4bb1ef6648f780cdd
Luis Otávio Batista <luisotaviodev@outlook.com>
2022-03-04 16:42:13 -0300

    task 0

---
+++ 0x22/armazem/script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+javac -d build com/holbertonschool/matematica/Aritmeticos.java
+cd /build
+jar -cf Matematica.jar
---
+++ 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -0,0 +1,23 @@
+package com.holbertonschool.matematica;
+
+public class Aritmeticos {
+	
+	
+	    public static int somar(int num1, int num2) {
+	        return num1 + num2;
+	    }
+
+	    public static int subtrair(int num1, int num2) {
+	        return num1 - num2;
+	    }
+
+	    public static int multiplicar(int num1, int num2) {
+	        return num1 * num2;
+	    }
+
+	    public static int dividir(int num1, int num2) {
+	        return num1 / num2;
+	    
+	}
+}
+
